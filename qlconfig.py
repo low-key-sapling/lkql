b6c5608ede4e06cee1eb54e616759520dd4fa327
低调D树苗 <yuanjifan@foxmail.com>
2024-09-18 16:02:29 +0800

    青龙配置文件API

---
+++ qlconfig.py
@@ -0,0 +1,61 @@
+import json
+
+from .ql import ql_api
+
+
+class qlconfig(ql_api):
+    """
+    青龙面板api配置文件模块
+
+    url: 青龙面板IP地址(不包含http://)
+
+    post: 青龙面板端口
+
+    client_id: 青龙面板openapi登录用户名
+
+    client_secret: 青龙面板openapi登录密码
+
+    Usage::
+        >>> ql_config = qlconfig(
+            url="12.22.43.23",
+            port=5700,
+            client_id="admin",
+            client_secret="abcdefg_",
+        )
+        ql_config.list()
+    """
+    def __init__(self, url: str, post: int, client_id: str, client_secret: str):
+        super().__init__(url, post, client_id, client_secret)
+
+  def list(self):
+        """获取配置文件列表
+
+        :return: 源相应json
+        """
+        url = f"{self.url}/open/configs/files"
+        return self.s.get(url=url).json()
+
+
+   def value(self, file_name):
+        """获取配置文件内容
+
+        :param file_name: 文件名
+        :return: 源相应json
+        """
+        url = f"{self.url}/open/configs/{file_name}"
+        return self.s.get(url=url).json()
+
+
+  def save(self, name, content):
+        """保存配置文件
+
+        :param name: 文件名
+        :param content: 值
+        :return: 源相应json
+        """
+        url = f"{self.url}/open/configs/save"
+        data = {
+            "name": name,
+            "content": content
+        }
+        return self.s.post(url=url, data=json.dumps(data)).json()
